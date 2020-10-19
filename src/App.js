d50e2f9e803493426a1d6ac5e8ad723f03a078f2
jaeyong <maxwodyd98@gmail.com>
2020-10-19 18:45:24 +0900

    UPDATE: 웹과 모바일 미디어쿼리로 분리

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { StartPage, GuidePage, Page1_1, Page1_2, Page1_3, Page2_1, Page2_2, Page2_3, Page2_4, Page2_5 } from './pages';
 export default function App() {
   return (
-    <Wrapper>
-      <Route exact path="/" component={StartPage} />
-      <Route path="/0" component={GuidePage} />
+    <Fragment>
+      <Wrapper>
+        <Switch>
+          <Route exact path="/" component={StartPage} />
+          <Route path="/0" component={GuidePage} />
 
-      {/* Page 1 */}
-      <Route path="/1" component={Page1_1} />
-      <Route path="/2" component={Page1_2} />
-      <Route path="/3" component={Page1_3} />
+          {/* Page 1 */}
+          <Route path="/1" component={Page1_1} />
+          <Route path="/2" component={Page1_2} />
+          <Route path="/3" component={Page1_3} />
 
-      {/* Page 2 */}
-      <Route path="/4" component={Page2_1} />
-      <Route path="/5" component={Page2_2} />
-      <Route path="/6" component={Page2_3} />
-      <Route path="/7" component={Page2_4} />
-      <Route path="/8" component={Page2_5} />
-      {/* <Route path="/9" component={} />
-      <Route path="/10" component={} />
-      <Route path="/11" component={} />
-      <Route path="/12" component={} /> */}
-    </Wrapper>
+          {/* Page 2 */}
+          <Route path="/4" component={Page2_1} />
+          <Route path="/5" component={Page2_2} />
+          <Route path="/6" component={Page2_3} />
+          <Route path="/7" component={Page2_4} />
+          <Route path="/8" component={Page2_5} />
+          {/* <Route path="/9" component={} />
+          <Route path="/10" component={} />
+          <Route path="/11" component={} />
+          <Route path="/12" component={} /> */}
+        </Switch>
+      </Wrapper>
+      <Web>모바일에서 이용해주세요.</Web>
+    </Fragment>
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  @media screen and (min-width: 400px) {
+    display: none;
+    z-index: 0;
+  }
+`;
+
+const Web = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-color: black;
+  color: white;
+
+  @media screen and (max-width: 400px) {
+    display: none;
+    z-index: 0;
+  }
+`;
