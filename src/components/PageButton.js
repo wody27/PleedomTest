c2bf578061ca3938b28634b2c1e4e86279681e4a
jaeyong <maxwodyd98@gmail.com>
2020-11-05 13:39:26 +0900

    PageButton 아이폰 레이아웃 적용

--- src/components/PageButton.js
+++ src/components/PageButton.js
@@ -49,21 +49,64 @@ export default function PageButton({ pageNumber, location, number, selected, que
 
 const LeftButton = styled.img`
   position: absolute;
+
   top: 40px;
-  @media screen and (max-width: 321px) {
+  left: 8px;
+
+  // 더 작은 사이즈들
+  @media (max-height: 568px) {
     top: 31px;
     left: 3px;
+  }
+
+  // 아이폰 6,7,8,SE 사이즈
+  @media (min-height: 569px) and (max-height: 669px) {
+    top: 40px;
+    left: 6px;
+  }
+
+  // 아이폰 plus 사이즈
+  @media (min-height: 670px) and (max-height: 736px) {
+    top: 47px;
+    left: 10px;
+  }
+
+  // 아이폰 X, XS 사이즈
+  @media (min-height: 737px) and (max-height: 812px) {
+    top: 40px;
+    left: 6px;
   }
 
   z-index: 2;
 `;
 const RightButton = styled.img`
   position: absolute;
+
   top: 40px;
   right: 8px;
-  @media screen and (max-width: 321px) {
+
+  // 더 작은 사이즈들
+  @media (max-height: 568px) {
     top: 31px;
     right: 3px;
+  }
+
+  // 아이폰 6,7,8,SE 사이즈
+  @media (min-height: 569px) and (max-height: 669px) {
+    top: 40px;
+    right: 6px;
+  }
+
+  // 아이폰 plus 사이즈
+  @media (min-height: 670px) and (max-height: 736px) {
+    top: 47px;
+    right: 10px;
+  }
+
+  // 아이폰 X, XS 사이즈
+  @media (min-height: 737px) and (max-height: 812px) {
+    top: 40px;
+    right: 6px;
   }
 
   z-index: 2;
