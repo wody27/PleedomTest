e1cf20fabcb87308d1366fbd154aabcb10c90214
jaeyong <maxwodyd98@gmail.com>
2020-11-06 14:14:19 +0900

    반응별 폰트 적용

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -24,24 +24,29 @@ const Wrapper = styled.div`
   font-stretch: normal;
   font-style: normal;
 
-  // 더 작은 사이즈들
-  @media (max-height: 568px) {
+  // 아이폰 5
+  @media (max-width: 330px) {
     font-size: 12px !important;
   }
 
-  // 아이폰 6,7,8,SE 사이즈
-  @media (min-height: 569px) and (max-height: 668px) {
-    font-size: 15px !important;
-  }
-
-  // 아이폰 plus 사이즈
-  @media (min-height: 669px) and (max-height: 736px) {
+  // Galaxy S5, Moto G4
+  @media (min-width: 350px) and (max-width: 370px) {
     font-size: 14px !important;
   }
 
-  // 아이폰 X, XS 사이즈
-  @media (min-height: 737px) and (max-height: 812px) {
+  // 아이폰 6,7,8,SE,X,XS 사이즈
+  @media (min-width: 365px) and (max-width: 385px) {
     font-size: 15px !important;
+  }
+
+  // 아이폰 11, plus 사이즈, Pixel ,Pixel2 XL 사이즈
+  @media (min-width: 404px) and (max-width: 424px) {
+    font-size: 14px !important;
+  }
+
+  // 아이폰 12 Pro Max
+  @media (min-width: 428px) {
+    font-size: 16px !important;
   }
 
   line-height: 1.5;
