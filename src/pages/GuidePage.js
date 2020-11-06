df3c7881682714046a849d3406af0e6923a811b0
jaeyong <maxwodyd98@gmail.com>
2020-11-06 14:19:02 +0900

    아이폰 6,7,8 화살표 레이아웃 적용

--- src/pages/GuidePage.js
+++ src/pages/GuidePage.js
@@ -46,33 +46,33 @@ const LeftButton = styled.img`
   top: 40px;
   left: 8px;
 
-  // 더 작은 사이즈들
-  @media (max-height: 568px) {
+  // 아이폰 5
+  @media (max-width: 330px) {
     top: 31px;
     left: 3px;
   }
 
-  // 아이폰 6,7,8,SE 사이즈
-  @media (min-height: 569px) and (max-height: 669px) {
+  // Galaxy S5, Moto G4
+  @media (min-width: 350px) and (max-width: 370px) {
+    top: 37px;
+    left: 6px;
+  }
+
+  // 아이폰 6,7,8,SE,X,XS 사이즈
+  @media (min-width: 365px) and (max-width: 385px) {
     top: 40px;
     left: 6px;
   }
 
-  // 아이폰 plus 사이즈
-  @media (min-height: 670px) and (max-height: 736px) {
+  // 아이폰 11, plus 사이즈, Pixel ,Pixel2 XL 사이즈
+  @media (min-width: 404px) and (max-width: 424px) {
     top: 47px;
     left: 10px;
   }
 
-  // 아이폰 X, XS 사이즈
-  @media (min-height: 737px) and (max-height: 812px) {
-    top: 40px;
-    left: 6px;
-  }
-
-  // Pixel 2 XL 사이즈
-  @media (min-height: 814px) {
-    top: 47px;
+  // 아이폰 12 Pro Max
+  @media (min-width: 428px) {
+    top: 48px;
     left: 10px;
   }
 `;
@@ -83,33 +83,33 @@ const RightButton = styled.img`
   top: 40px;
   right: 8px;
 
-  // 더 작은 사이즈들
-  @media (max-height: 568px) {
+  // 아이폰 5
+  @media (max-width: 330px) {
     top: 31px;
     right: 3px;
   }
 
-  // 아이폰 6,7,8,SE 사이즈
-  @media (min-height: 569px) and (max-height: 669px) {
+  // Galaxy S5, Moto G4
+  @media (min-width: 350px) and (max-width: 370px) {
+    top: 37px;
+    right: 6px;
+  }
+
+  // 아이폰 6,7,8,SE,X,XS 사이즈
+  @media (min-width: 365px) and (max-width: 385px) {
     top: 40px;
     right: 6px;
   }
 
-  // 아이폰 plus 사이즈
-  @media (min-height: 670px) and (max-height: 736px) {
+  // 아이폰 11, plus 사이즈, Pixel ,Pixel2 XL 사이즈
+  @media (min-width: 404px) and (max-width: 424px) {
     top: 47px;
     right: 10px;
   }
 
-  // 아이폰 X, XS 사이즈
-  @media (min-height: 737px) and (max-height: 812px) {
-    top: 40px;
-    right: 6px;
-  }
-
-  // Pixel 2 XL 사이즈
-  @media (min-height: 814px) {
-    top: 47px;
+  // 아이폰 12 Pro Max
+  @media (min-width: 428px) {
+    top: 48px;
     right: 10px;
   }
 `;
